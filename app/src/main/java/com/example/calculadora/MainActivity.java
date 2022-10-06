5b0662b2f332545d76c6d32c4560e2dcc8bad5bf
nieto010 <janietoc04@educarex.es>
2022-10-06 20:46:07 +0200

    commit 3

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -4,14 +4,22 @@ package com.example.calculadora;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    public double contador;
+
     EditText operando1, operando2;
     TextView textoResultado;
+    Spinner spinner;
+    double valor1;
+    double valor2;
+    double resultado = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,43 +27,58 @@ public class MainActivity extends Activity {
         operando1 = (EditText)findViewById(R.id.numero1);
         operando2 = (EditText)findViewById(R.id.numero2);
         textoResultado = (TextView) findViewById(R.id.contador);
-        contador = 0;
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 
     public void sumar(View vista) {
-        double valor1 = Double.parseDouble(operando1.getText().toString());
-        double valor2 = Double.parseDouble(operando2.getText().toString());
-        double resultado = valor1 + valor2;
+
+
+        resultado = valor1 + valor2;
 
         textoResultado.setText(" " + resultado);
     }
 
     public void restar(View vista) {
-        double valor1 = Double.parseDouble(operando1.getText().toString());
-        double valor2 = Double.parseDouble(operando2.getText().toString());
-        double contador = valor1 - valor2;
+        resultado = valor1 - valor2;
 
-        textoResultado.setText(" " + contador);
+        textoResultado.setText(" " + resultado);
     }
 
     public void multiplicar(View vista) {
-        double valor1 = Double.parseDouble(operando1.getText().toString());
-        double valor2 = Double.parseDouble(operando2.getText().toString());
-        double contador = valor1 * valor2;
+        resultado = valor1 * valor2;
 
-        textoResultado.setText(" " + contador);
+        textoResultado.setText(" " + resultado);
     }
 
     public void dividir(View vista) {
-        double valor1 = Double.parseDouble(operando1.getText().toString());
-        double valor2 = Double.parseDouble(operando2.getText().toString());
-        do{
-            double contador = valor1 / valor2;
-        }while(valor1 != 0);
-        if(valor1 == 0){
-            contador = 0;
+        if (valor1 == 0 && valor2 == 0 || valor2 == 0) {
+            Toast.makeText(this,"No se puede realizar esta división",Toast.LENGTH_LONG).show();
         }
-        textoResultado.setText(" " + contador);
-    }
+        else {
+            resultado = valor1 / valor2;
+        }
 
+        textoResultado.setText(" " + resultado);
+    }
+    public void calcular(View vista) {
+        valor1 = Double.parseDouble(operando1.getText().toString());
+        valor2 = Double.parseDouble(operando2.getText().toString());
+        if (spinner.getSelectedItem().toString().equals("Sumar")) {
+            sumar(null);
+        }
+        if (spinner.getSelectedItem().toString().equals("Restar")) {
+            restar(null);
+        }
+        if (spinner.getSelectedItem().toString().equals("Multiplicar")) {
+            multiplicar(null);
+        }
+        if (spinner.getSelectedItem().toString().equals("Dividir")) {
+            dividir(null);
+        }
+    }
 }
